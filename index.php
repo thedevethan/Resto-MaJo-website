7d8c5f667e903dfca5ba542639dd2c5362682418
ethanbokame <170204507+thedevethan@users.noreply.github.com>
2024-06-19 00:25:31 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php require("header.php"); ?>
+
+<body>
+
+    <header>
+        <?php require("navbar.php"); ?>
+    </header>
+
+    <?php require("firstview.php")?>
+    
+    <?php require("menu.php")?>
+
+    <?php require("newsletter.php"); ?>
+
+    <?php require("footer.php"); ?>
+
+
+</body>
+
+</html>
